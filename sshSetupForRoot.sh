28b5be9bdd8b4612bb1a367706be3dcf79dec821
thanhtam <vutam1412@gmail.com>
2022-06-09 00:19:02 +0700

    updtae link

--- sshSetupForRoot.sh
+++ sshSetupForRoot.sh
@@ -3,16 +3,16 @@ cd /etc/ssh
 rm -rf /etc/ssh/sshd_config
 
 
-curl -o sshd_config https://raw.githubusercontent.com/thanhtamkaito/LinuxSetting/setup/sshd_config?token=GHSAT0AAAAAABVDEONCKZ774UEVU5LPOT6QYVAZFZA
+curl -o sshd_config https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/sshd_config
 
 cd /root/.ssh
 
 rm -rf /root/.ssh/authorized_keys
-curl -o /root/.ssh/authorized_keys https://raw.githubusercontent.com/thanhtamkaito/LinuxSetting/setup/id_rsa.pub?token=GHSAT0AAAAAABVDEONDHXPDFEG2W3PYVX36YVAZSJA
+curl -o /root/.ssh/authorized_keys https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/id_rsa.pub
 
 sudo chmod 700 /root
 sudo chmod 700 /root/.ssh 
-sudo chmod 600 /root/.ssh/authorized_keys 
+sudo chmod 660 /root/.ssh/authorized_keys 
 
 systemctl restart sshd
 systemctl status sshd
--- sshSetupForUser.sh
+++ sshSetupForUser.sh
@@ -4,22 +4,20 @@ cd /etc/ssh
 rm -rf /etc/ssh/sshd_config
 
 
-curl -o sshd_config https://raw.githubusercontent.com/thanhtamkaito/LinuxSetting/setup/sshd_config?token=GHSAT0AAAAAABVDEONCKZ774UEVU5LPOT6QYVAZFZA
-
-
+curl -o sshd_config https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/sshd_config
 
 cd /home/ubuntu/.ssh
 
 rm -rf /home/ubuntu/.ssh/authorized_keys
-curl -o /home/ubuntu/.ssh/authorized_keys https://raw.githubusercontent.com/thanhtamkaito/LinuxSetting/setup/id_rsa.pub?token=GHSAT0AAAAAABVDEONDHXPDFEG2W3PYVX36YVAZSJA
-
-vi /home/ubuntu/.ssh/authorized_keys
+curl -o /home/ubuntu/.ssh/authorized_keys https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/id_rsa.pub
 
 sudo chmod 700 /home/ubuntu
 sudo chmod 700 /home/ubuntu/.ssh
-sudo chmod 600 /home/ubuntu/.ssh/authorized_keys
-sudo chown -R  root:ubuntu /home/ubuntu/.ssh/authorized_keys
+sudo chmod 660 /home/ubuntu/.ssh/authorized_keys
+sudo chown -R root:ubuntu /home/ubuntu/.ssh/authorized_keys
 
 systemctl restart sshd
 systemctl status sshd
+
+
 
