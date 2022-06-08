514c2f3fd5b76c593975ee1727d1db6b2025398b
thanhtam <vutam1412@gmail.com>
2022-06-08 22:22:56 +0700

    update for NEOVIM

--- install.sh
+++ install.sh
@@ -1,29 +1,39 @@
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt install python3-pip 
+sudo apt-get upgrade -y
+sudo apt install curl -y
+sudo apt -y install gcc g++ make -y
+sudo apt install nodejs -y
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+sudo apt install nodejs -y
+sudo apt install npm
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt install python3.9 -y
+sudo apt install python3-pip -y
 sudo apt install net-tools
 sudo apt install pacman -y
-sudo apt-get install openssh-server -y 
+sudo apt-get install openssh-server -y
 sudo apt install git-all -y
 sudo apt install tar -y
 sudo apt install unzip -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-sudo apt -y install nodejs 
 sudo apt install ripgrep -y
 sudo apt install pacman -y
-sudo apt-get install ctags  -y
-pip  install pynvim  
-sudo apt install python3-pip  -y
+sudo apt install python3-pip -y
 sudo apt-get install pip -y
 sudo apt install neovim -y
+sudo npm install neovim -g
+sudo apt-get install ruby-neovim -y
+pip3 install pynvim
+pip3 install --upgrade pynvim
+sudo apt install xsel
 sudo apt-get install python-all -y
-sudo apt install npm -y
 sudo apt-get install gcc -y
-sudo apt install nodejs  -y
 sudo apt-get install nmap -y
 sudo apt install netcat-traditional -y
 sudo apt install unicorn -y
-npm  install neovim -g
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
