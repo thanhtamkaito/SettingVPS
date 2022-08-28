ab56d529ffdb709ce12b12f7bb9315f2e0d78d64
thanhtamkaito <49809669+thanhtamkaito@users.noreply.github.com>
2022-08-29 00:28:58 +0700

    update

--- settingWebSite.sh
+++ settingWebSite.sh
@@ -2,8 +2,10 @@
 apt update
 apt install apache2 -y
 cd /var/www/html/
+rm -rf /var/www/html/*
 wget https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/index.html
 mkdir -p product
 cd product
+rm -rf ./product
 wget https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/index.html
 cd ~
