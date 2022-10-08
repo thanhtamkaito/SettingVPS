fac4adb16e9daa3ab7fdb1d158daf0107f332259
thanhtamkaito <49809669+thanhtamkaito@users.noreply.github.com>
2022-10-08 11:12:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,6 +33,13 @@ sudo apt-get install nmap -y
 sudo apt install netcat-traditional -y
 sudo apt install unicorn -y
 
+sudo apt install openjdk-11-jdk -y
+wget -p -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+
+ 
 sudo apt  install docker.io -y
 sudo apt  install docker-compose -y
 sudo groupadd docker
