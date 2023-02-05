bb6fae4e43c82c705d3839e00743b21f9fb64689
thanhtamkaito <49809669+thanhtamkaito@users.noreply.github.com>
2023-02-05 16:13:15 +0700

    Create openvpn.sh

---
+++ openvpn.sh
@@ -0,0 +1,39 @@
+
+#https://www.cyberciti.biz/faq/howto-setup-openvpn-server-on-ubuntu-linux-14-04-or-16-04-lts/
+
+wget https://git.io/vpn -O openvpn-install.sh
+sudo chmod +x openvpn-install.sh
+sudo bash openvpn-install.sh
+
+sudo systemctl restart openvpn-server@server.service
+sudo find / -type f -name "kaito.ovpn"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
