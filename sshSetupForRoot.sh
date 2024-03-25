22dd328e08634b4648857e58f2fda72014f018da
Vũ Thành Tâm <49809669+thanhtamkaito@users.noreply.github.com>
2024-03-25 09:00:52 +0700

    Update sshSetupForRoot.sh

--- sshSetupForRoot.sh
+++ sshSetupForRoot.sh
@@ -1,4 +1,7 @@
 
+cd /etc/ssh
+rm -rf /etc/ssh/sshd_config
+curl -o sshd_config https://raw.githubusercontent.com/thanhtamkaito/SettingVPS/setup/sshd_config
 
 cd /root/.ssh
 rm -rf /root/.ssh/authorized_keys
